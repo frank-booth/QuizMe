b57ce172b96338bbd0ede0ca1214836e6df89d64
frank-booth <jfraley519@gmail.com>
2022-11-08 12:05:09 -0500

    bootstrap working

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import './App.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <header className="App-header">
-        <h1>Hello World!</h1>
+        <h1 className="text-primary">Hello World!</h1>
       </header>
     </div>
   )
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import './index.css'
 import App from './App'
 
